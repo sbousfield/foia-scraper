39f160b0791a4010ff8458eb9fb2ca516af71be2
sbousfield <43867719+sbousfield@users.noreply.github.com>
2026-03-03 19:22:53 +1100

    finalised most of the pdf_extraction methodologies (lots of cells and trial and error in notebook), started to build a reusable class in a .py

--- src/config.py
+++ src/config.py
@@ -11,6 +11,7 @@ SOURCES = {
 PROJECT_ROOT = Path(__file__).parent.parent.resolve()
 DOWNLOAD_DIR = PROJECT_ROOT/'data/raw'
 METADATA_FILE = PROJECT_ROOT/'data/metadata/scraped_docs.csv'
+PROCESSED_DIR = PROJECT_ROOT/'data/processed-data'
 DELAY_BETWEEN_REQUESTS = 2  # seconds - be polite to servers
 MAX_RETRIES = 3
 MAX_DOCUMENTS = 50  # Limit for initial scraping
